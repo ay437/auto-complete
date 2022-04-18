260953cba5aeee20cec528c966c86cef4303c5ea
Arjun Yadav <ayadav@and.digital>
2022-04-18 16:48:14 +0100

    Doing cleanup on create-react-app, updating readme

---
+++ src/AutoComplete.tsx
@@ -0,0 +1,11 @@
+import './AutoComplete.css';
+
+function AutoComplete() {
+  return (
+    <div className="Auto-complete">
+      This is the auto complete field for capital cities of the world
+    </div>
+  );
+}
+
+export default AutoComplete;
